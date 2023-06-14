76c72b79caec811cc609e443f57810973ab8b9e1
Daniel Lima Azevedo <620.406.143-70@dpema.br>
2023-06-14 12:54:09 -0300

    implementação de camada de serviço

---
+++ src/main/java/padroes/etapa3/parte2/service/LocacaoService.java
@@ -0,0 +1,44 @@
+
+package padroes.etapa3.parte2.service;
+
+import java.util.List;
+import padroes.etapa3.parte2.models.Entity;
+import padroes.etapa3.parte2.repository.LocacaoRepository;
+
+/**
+ *
+ * @author daniel korban
+ */
+public class LocacaoService {
+    
+    private final LocacaoRepository locacaoRepository;
+
+    public LocacaoService(LocacaoRepository LocacaoRepository) {
+        this.locacaoRepository = LocacaoRepository;
+    }
+    
+    public List<Entity> listarTodos(){
+       return locacaoRepository.buscarTodos();
+    }
+    
+    public Entity recuperar(int id){
+        return locacaoRepository.recuperar(id);
+    }
+    
+    public void cadastrar(Entity entity){
+        locacaoRepository.cadastrar(entity);
+    }
+    
+    public void atualizar(Entity entity){
+        locacaoRepository.atualizar(entity);
+    }
+    
+    public void excluirPorNome(Entity entity){
+        locacaoRepository.excluirPorNome(entity);
+    }
+    
+    public void excluirPorId(int id){
+        locacaoRepository.excluirPorId(id);
+    }
+    
+}
